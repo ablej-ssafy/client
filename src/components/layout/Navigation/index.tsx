8567d4ad3a234cd7bab5fb87c71bd02f09b1692b
BHyeonKim <rlaqhguse@gmail.com>
2024-10-22 22:39:26 +0900

    [#2]feat: 네비게이션 애니메이션 적용
    하나의 viewport height를 넘어갈시 background color를 primary color로 변경함

--- src/components/layout/Navigation/index.tsx
+++ src/components/layout/Navigation/index.tsx
@@ -1,19 +1,52 @@
+'use client';
+
 import classNames from 'classnames/bind';
+import type {Variants} from 'framer-motion';
+import * as motion from 'framer-motion/client';
 import Link from 'next/link';
-import {PropsWithChildren} from 'react';
+import {PropsWithChildren, useEffect, useRef, useState} from 'react';
+
+import useScrollHeight from '@/hooks/useScrollHeight';
 
 import styles from './navigation.module.scss';
 
 const cx = classNames.bind(styles);
 
+const navVarients: Variants = {
+  normal: {
+    transition: {duration: 0.3},
+  },
+  inverted: {
+    backgroundColor: '#6e55ff',
+    transition: {duration: 0.3},
+  },
+};
+
 const Navigation = ({children}: PropsWithChildren) => {
+  const windowHeight = useRef(window.innerHeight);
+  const scrollHeight = useScrollHeight();
+  const [inverted, setInverted] = useState(false);
+
+  useEffect(() => {
+    if (scrollHeight >= windowHeight.current) {
+      setInverted(true);
+    } else {
+      setInverted(false);
+    }
+  }, [scrollHeight]);
+
   return (
-    <nav className={cx('navigation')}>
+    <motion.nav
+      className={cx('navigation', {inverted: inverted})}
+      variants={navVarients}
+      initial={'normal'}
+      animate={inverted ? 'inverted' : 'normal'}
+    >
       <Link className={cx('title')} href={'/'}>
         AI HeadHunting
       </Link>
       <ul className={cx('nav-items')}>{children}</ul>
-    </nav>
+    </motion.nav>
   );
 };
 
