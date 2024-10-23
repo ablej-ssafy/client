bf099e3fb549ecce6a68c01a3e3ef8d45f31a50a
BHyeonKim <rlaqhguse@gmail.com>
2024-10-23 17:53:35 +0900

    [#2]feat: 네번째 섹션에 애니메이션 추가

--- src/features/Landing/IconCard/index.tsx
+++ src/features/Landing/IconCard/index.tsx
@@ -1,10 +1,10 @@
 import classNames from 'classnames/bind';
+import type {Variants} from 'framer-motion';
+import * as motion from 'framer-motion/client';
 import Image, {ImageProps} from 'next/image';
 import {ReactNode} from 'react';
 
 import styles from './iconCard.module.scss';
-
-const cx = classNames.bind(styles);
 
 interface LottieCardProps {
   title: string;
@@ -12,15 +12,30 @@ interface LottieCardProps {
   imageOption: ImageProps;
 }
 
+const cx = classNames.bind(styles);
+
+const variants: Variants = {
+  hidden: {opacity: 0, y: -100},
+  visible: {opacity: 1, y: 0, transition: {duration: 1}},
+};
+
 const IconCard = ({imageOption, description, title}: LottieCardProps) => {
   return (
-    <div className={cx('card')}>
+    <motion.div
+      className={cx('card')}
+      variants={variants}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{once: true, margin: '-200px'}}
+    >
       <Image {...imageOption} className={cx('image')} />
-      <div>
-        <h2>{title}</h2>
-        <p className={cx('paragraph')}>{description}</p>
-      </div>
-    </div>
+      <motion.div variants={variants}>
+        <motion.h2 variants={variants}>{title}</motion.h2>
+        <motion.p variants={variants} className={cx('paragraph')}>
+          {description}
+        </motion.p>
+      </motion.div>
+    </motion.div>
   );
 };
 
