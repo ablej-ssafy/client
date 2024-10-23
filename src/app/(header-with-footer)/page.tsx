4d995d28b65dcc87eb35af07fa06445e6677665d
BHyeonKim <rlaqhguse@gmail.com>
2024-10-23 10:44:30 +0900

    [#2]feat: 랜딩페이지 세번째 섹션 추가

--- src/app/(header-with-footer)/page.tsx
+++ src/app/(header-with-footer)/page.tsx
@@ -1,11 +1,13 @@
 import MainSection from '@/features/Landing/Sections/MainSection';
 import SecondSection from '@/features/Landing/Sections/SecondSection';
+import ThirdSection from '@/features/Landing/Sections/ThirdSection';
 
 export default function Home() {
   return (
     <main>
       <MainSection />
       <SecondSection />
+      <ThirdSection />
     </main>
   );
 }
---
+++ src/features/Landing/Sections/ThirdSection/index.tsx
@@ -0,0 +1,17 @@
+import classNames from 'classnames/bind';
+
+import DescriptionCard from '@/features/Landing/DescriptionCard';
+
+import styles from './thirdSection.module.scss';
+
+const cx = classNames.bind(styles);
+
+const ThirdSection = () => {
+  return (
+    <section className={cx('section')}>
+      <DescriptionCard />
+    </section>
+  );
+};
+
+export default ThirdSection;
