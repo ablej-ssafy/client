3e640535ab825c4bdb9ea0f9dea4fbdf6acca507
yunhayeon <yhy5049@naver.com>
2024-10-23 14:10:49 +0900

    [#4]feat: 이력서 페이지 레이아웃 작업 완료

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,17 @@
+'use Client';
+
+import React from 'react';
+
+import MyResume from '@/features/resume/MyResume';
+import ResumeRegistration from '@/features/resume/ResumeRegistration';
+
+const ResumePage = () => {
+  return (
+    <>
+      <ResumeRegistration />
+      <MyResume />
+    </>
+  );
+};
+
+export default ResumePage;
