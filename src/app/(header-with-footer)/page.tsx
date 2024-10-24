337bff12f325b2ec79197d5a03ee29f7d0a672ae
BHyeonKim <rlaqhguse@gmail.com>
2024-10-24 15:47:12 +0900

    fix: 경로 수정

--- src/app/(header-with-footer)/page.tsx
+++ src/app/(header-with-footer)/page.tsx
@@ -1,9 +1,9 @@
 import Footer from '@/components/layout/Footer';
-import FifthSection from '@/features/temp/Sections/FifthSection';
-import FourthSection from '@/features/temp/Sections/FourthSection';
-import MainSection from '@/features/temp/Sections/MainSection';
-import SecondSection from '@/features/temp/Sections/SecondSection';
-import ThirdSection from '@/features/temp/Sections/ThirdSection';
+import FifthSection from '@/features/landing/Sections/FifthSection';
+import FourthSection from '@/features/landing/Sections/FourthSection';
+import MainSection from '@/features/landing/Sections/MainSection';
+import SecondSection from '@/features/landing/Sections/SecondSection';
+import ThirdSection from '@/features/landing/Sections/ThirdSection';
 
 export default function Home() {
   return (
--- src/features/landing/Sections/ThirdSection/index.tsx
+++ src/features/landing/Sections/ThirdSection/index.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames/bind';
 
-import DescriptionCard from '@/features/temp/DescriptionCard';
+import DescriptionCard from '@/features/landing/DescriptionCard';
 
 import styles from './thirdSection.module.scss';
 
