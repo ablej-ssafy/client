91ad86ad2a56646f93140546a74ebcd0a9dea734
BHyeonKim <rlaqhguse@gmail.com>
2024-10-24 11:56:11 +0900

    [#2]feat: 글래스카드에 애니메이션 추가

--- src/features/Landing/GlassCard/index.tsx
+++ src/features/Landing/GlassCard/index.tsx
@@ -1,23 +1,52 @@
 import classNames from 'classnames/bind';
+import type {Variants} from 'framer-motion';
+import * as motion from 'framer-motion/client';
 
 import styles from './glassCard.module.scss';
 
 const cx = classNames.bind(styles);
 
+const variants: Variants = {
+  hidden: {
+    opacity: 0,
+    y: -100,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {duration: 1},
+  },
+};
+
 const GlassCard = () => {
   return (
-    <div className={cx('glass-card')}>
-      <div className={cx('card-section')}>
-        <p>
-          지금까지 <span className={cx('strong')}>AI HeadHunting</span>을 사용해
-          취업한 <span>구직자 수</span>
-        </p>
-      </div>
-      <div className={cx('card-section')}>
-        <p>
-          <span className={cx('number')}>10000</span>+
-        </p>
-      </div>
+    <div className={cx('container')}>
+      <motion.div
+        className={cx('glass-card')}
+        variants={variants}
+        initial="hidden"
+        whileInView="animate"
+        viewport={{once: true, margin: '-394px'}}
+      >
+        <motion.div className={cx('card-section')} variants={variants}>
+          <motion.p variants={variants}>
+            지금까지{' '}
+            <motion.span className={cx('strong')} variants={variants}>
+              AI HeadHunting
+            </motion.span>
+            을 사용해 취업한{' '}
+            <motion.span variants={variants}>구직자 수</motion.span>
+          </motion.p>
+        </motion.div>
+        <motion.div className={cx('card-section')} variants={variants}>
+          <motion.p variants={variants}>
+            <motion.span className={cx('number')} variants={variants}>
+              10000
+            </motion.span>
+            +
+          </motion.p>
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
