ab6dae28af8753cfcf891870a545c63f0044f6a2
김도영 <rlaehdud1002@naver.com>
2024-10-25 16:35:51 +0900

    [#5]fix: pr comment 반영

--- src/app/(header-with-footer)/layout.tsx
+++ src/app/(header-with-footer)/layout.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import {ReactNode} from 'react';
 
 import ModalProvider from '@/components/layout/Modal/ModalProvider';
--- src/features/signin/SignIn/SignModal.tsx
+++ src/features/signin/SignIn/SignModal.tsx
@@ -16,9 +16,11 @@ export enum SIGN_STEP {
 
 const SignModal = () => {
   const [step, setStep] = useState<SIGN_STEP>(SIGN_STEP.SIGNIN);
+
   return (
     <div className={styles['sign-modal']}>
       {step === SIGN_STEP.SIGNIN && <SigninInput setStep={setStep} />}
+
       {step === SIGN_STEP.USERINFO && (
         <>
           <p className={styles['text-header']}>이제 곧 시작</p>
@@ -29,14 +31,15 @@ const SignModal = () => {
           <InfoInput setStep={setStep} />
         </>
       )}
+
       {step === SIGN_STEP.CAREER && (
-        <div>
+        <>
           <p className={styles['text-header']}>마지막 한 단계</p>
           <p className={styles['text-content']}>
             회원님의 경력과 관심 직무를 선택해주세요.
           </p>
           <CareerInput />
-        </div>
+        </>
       )}
     </div>
   );
