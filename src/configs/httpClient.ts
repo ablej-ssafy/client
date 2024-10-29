856cc0f02ec435a6210a3da9353ee8cc507ef8b4
BHyeonKim <rlaqhguse@gmail.com>
2024-10-29 09:23:40 +0900

    [#33]feat: 직렬화 로직 추가

--- src/configs/httpClient.ts
+++ src/configs/httpClient.ts
@@ -1,7 +1,10 @@
+type CustomBody = BodyInit | object;
+type CustomOptions = Omit<RequestInit, 'body'> & {body?: CustomBody};
+
 class HttpClient {
   static BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
   private static instance: null | HttpClient = null;
-  private static httpClient: null | typeof fetch = null;
+  private static httpClient: typeof fetch;
 
   private constructor() {
     HttpClient.httpClient = fetch;
@@ -14,13 +17,46 @@ class HttpClient {
     return HttpClient.instance;
   }
 
+  private beforeRequest(options?: CustomOptions): RequestInit {
+    if (!options) {
+      return {};
+    }
+
+    if (options.body && options.body instanceof FormData) {
+      return {
+        ...options,
+        body: options.body,
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          ...options.headers,
+        },
+      };
+    }
+
+    if (options.body && typeof options.body === 'object') {
+      return {
+        ...options,
+        body: JSON.stringify(options.body),
+        headers: {
+          'Content-Type': 'application/json',
+          ...options.headers,
+        },
+      };
+    }
+
+    return {...options, body: options.body};
+  }
+
   /**
    * HTTP 요청을 보내는 함수
    * @param url 요청 URL
    * @param options fetch API의 RequestInit 객체
    */
-  get(url: string, options?: RequestInit) {
-    return HttpClient.httpClient!(HttpClient.BASE_URL + url, options);
+  get(url: string, options?: Omit<CustomOptions, 'body'>) {
+    return HttpClient.httpClient!(
+      HttpClient.BASE_URL + url,
+      this.beforeRequest(options),
+    );
   }
 
   /**
@@ -29,24 +65,32 @@ class HttpClient {
    * @param body 요청 바디
    * @param options fetch API의 RequestInit 객체
    */
-  post(url: string, body?: BodyInit, options?: Omit<RequestInit, 'body'>) {
-    return HttpClient.httpClient!(HttpClient.BASE_URL + url, {
-      ...options,
-      body,
-      method: 'POST',
-    });
+  post(url: string, body?: CustomBody, options?: CustomOptions) {
+    return HttpClient.httpClient!(
+      HttpClient.BASE_URL + url,
+      this.beforeRequest({
+        ...options,
+        body,
+        method: 'POST',
+      }),
+    );
   }
 
   /**
    * PUT 요청을 보내는 함수
    * @param url 요청 URL
+   * @param body 요청 바디
    * @param options fetch API의 RequestInit 객체
    */
-  put(url: string, options?: RequestInit) {
-    return HttpClient.httpClient!(HttpClient.BASE_URL + url, {
-      ...options,
-      method: 'PUT',
-    });
+  put(url: string, body?: CustomBody, options?: CustomOptions) {
+    return HttpClient.httpClient!(
+      HttpClient.BASE_URL + url,
+      this.beforeRequest({
+        ...options,
+        body,
+        method: 'PUT',
+      }),
+    );
   }
 
   /**
@@ -54,11 +98,14 @@ class HttpClient {
    * @param url 요청 URL
    * @param options fetch API의 RequestInit 객체
    */
-  delete(url: string, options?: RequestInit) {
-    return HttpClient.httpClient!(HttpClient.BASE_URL + url, {
-      ...options,
-      method: 'DELETE',
-    });
+  delete(url: string, options?: CustomOptions) {
+    return HttpClient.httpClient!(
+      HttpClient.BASE_URL + url,
+      this.beforeRequest({
+        ...options,
+        method: 'DELETE',
+      }),
+    );
   }
 }
 
