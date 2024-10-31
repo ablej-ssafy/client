7b7e49dcba9a4a1b01119e954f9e2ab97aadcc01
BHyeonKim <rlaqhguse@gmail.com>
2024-10-31 15:58:17 +0900

    [#43]fix: usePreventScroll 제거

--- src/app/@modal/layout.tsx
+++ src/app/@modal/layout.tsx
@@ -6,14 +6,11 @@ import {ReactNode, useRef} from 'react';
 import Backdrop from '@/components/common/Backdrop';
 import Modal from '@/components/common/Modal';
 import useClickOutside from '@/hooks/useClickOutside';
-import usePreventScroll from '@/hooks/usePreventScroll';
 
 const ModalLayout = ({children}: {children: ReactNode}) => {
   const router = useRouter();
   const ref = useRef<HTMLDivElement>(null);
   useClickOutside(ref, () => router.back());
-  usePreventScroll();
-
   return (
     <Backdrop>
       <Modal ref={ref}>{children}</Modal>
