6968cb4a9b40d06e8f491586748529591c27908a
BHyeonKim <rlaqhguse@gmail.com>
2024-10-31 11:10:57 +0900

    feat: 모달이 떴을 때 스크롤이 안되게 변경

--- src/app/@modal/layout.tsx
+++ src/app/@modal/layout.tsx
@@ -6,11 +6,13 @@ import {ReactNode, useRef} from 'react';
 import Backdrop from '@/components/common/Backdrop';
 import Modal from '@/components/common/Modal';
 import useClickOutside from '@/hooks/useClickOutside';
+import usePreventScroll from '@/hooks/usePreventScroll';
 
 const ModalLayout = ({children}: {children: ReactNode}) => {
   const router = useRouter();
   const ref = useRef<HTMLDivElement>(null);
   useClickOutside(ref, () => router.back());
+  usePreventScroll();
 
   return (
     <Backdrop>
