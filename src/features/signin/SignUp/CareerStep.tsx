f82a132e2aa9dfabfca63883876c24397e5a82dd
BHyeonKim <rlaqhguse@gmail.com>
2024-10-31 10:59:51 +0900

    style: 커리어 스탭 스타일 수정

--- src/features/signin/SignUp/CareerStep.tsx
+++ src/features/signin/SignUp/CareerStep.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import classNames from 'classnames/bind';
 import {useFormState} from 'react-dom';
 
 import signupAction from '@/actions/signupAction';
@@ -8,6 +9,8 @@ import {useSignupForm} from '@/context/signup-context';
 import JobComboBox from '@/features/signin/JobComboBox';
 
 import styles from './careerStep.module.scss';
+
+const cx = classNames.bind(styles);
 
 const INITIAL_STATE = {
   email: [],
@@ -23,13 +26,20 @@ const CareerStep = () => {
   const [prevForm] = useSignupForm();
 
   return (
-    <form className={styles['signup-form']} action={formAction}>
+    <form className={cx('signup-form')} action={formAction}>
+      <p className={cx('text-header')}>이제 곧 시작</p>
+      <p className={cx('text-content')}>얼마 안남았어요...!</p>
+      <p className={cx('text-content')}>
+        회원가입을 하시면 AI 큐레이팅을 사용할 수 있습니다.
+      </p>
       <input name="email" value={prevForm.email} readOnly hidden />
       <input name="password" value={prevForm.password} readOnly hidden />
       <input name="name" value={prevForm.name} readOnly hidden />
-      <JobComboBox />
-      <Slider max={25} name="age" value={0} fill label="경력" />
-      <button type="submit" className={styles.button}>
+      <div className={cx('input-container')}>
+        <JobComboBox />
+        <Slider max={25} name="age" value={0} fill label="경력" />
+      </div>
+      <button type="submit" className={cx('button')}>
         회원가입 완료
       </button>
     </form>
