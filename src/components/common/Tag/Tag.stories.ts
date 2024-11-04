192fab4f8d56f714bedf91c4dc663065e202ef79
김도영 <rlaehdud1002@naver.com>
2024-11-04 10:07:58 +0900

    [#41]design: tag component storybook 추가

---
+++ src/components/common/Tag/Tag.stories.ts
@@ -0,0 +1,26 @@
+import type {Meta, StoryObj} from '@storybook/react';
+
+import Tag from './index';
+
+const meta = {
+  title: 'Common/Tag',
+  component: Tag,
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    title: {
+      control: 'text',
+    },
+  },
+} satisfies Meta<typeof Tag>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    title: 'Default',
+  },
+};
