58bab831454236024823315a3d773cfe7276dba0
yunhayeon <yhy5049@naver.com>
2024-11-06 13:43:29 +0900

    [#39]feat: 이력서 미리보기 모달 구현 완료

--- src/features/resume/MyResumeCard/index.tsx
+++ src/features/resume/MyResumeCard/index.tsx
@@ -1,11 +1,13 @@
 'use client';
 
+import {useState} from 'react';
 import {BsTrash3} from 'react-icons/bs';
 import {IoIosArrowForward} from 'react-icons/io';
 
 import resumeDeleteAction from '@/actions/resume/resumeDeleteAction';
 import {revalidateResumePage} from '@/actions/resume/revalidatePathAction';
 
+import PreviewModal from './../PreViewModal';
 import styles from './myResumeCard.module.scss';
 
 interface ResumeCardProps {
@@ -17,6 +19,8 @@ interface ResumeCardProps {
 }
 
 const MyResumeCard = ({id, url, title, date, type}: ResumeCardProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const handleDelete = async () => {
     const response = await resumeDeleteAction(id);
     if (response.success) {
@@ -24,31 +28,45 @@ const MyResumeCard = ({id, url, title, date, type}: ResumeCardProps) => {
     }
   };
 
-  console.log(url);
+  const handleOpenPDF = () => {
+    setIsOpen(true);
+  };
+
+  const handleClosePDF = () => {
+    setIsOpen(false);
+  };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.top}>
-        {type ? (
-          <div className={styles.self}>{type}</div>
-        ) : (
-          <div className={styles.default}>기본 이력서</div>
-        )}
-        <div onClick={handleDelete}>
-          <BsTrash3 size={20} />
+    <>
+      <div className={styles.container} onClick={handleOpenPDF}>
+        <div className={styles.top}>
+          {type ? (
+            <div className={styles.self}>{type}</div>
+          ) : (
+            <div className={styles.default}>기본 이력서</div>
+          )}
+          <div
+            onClick={e => {
+              e.stopPropagation();
+              handleDelete();
+            }}
+          >
+            <BsTrash3 size={20} />
+          </div>
+        </div>
+        <div className={styles.center}>
+          <div>{title}</div>
+          <div>{date.replaceAll('-', '.')}</div>
+        </div>
+        <div className={styles.link}>
+          <span>
+            추천 기업
+            <IoIosArrowForward />
+          </span>
         </div>
       </div>
-      <div className={styles.center}>
-        <div>{title}</div>
-        <div>{date.replaceAll('-', '.')}</div>
-      </div>
-      <div className={styles.link}>
-        <span>
-          추천 기업
-          <IoIosArrowForward />
-        </span>
-      </div>
-    </div>
+      <PreviewModal isOpen={isOpen} onClose={handleClosePDF} pdfUrl={url} />
+    </>
   );
 };
 
