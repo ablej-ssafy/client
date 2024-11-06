538f7fb46c25017b7a98c03ab54b5f83258dc989
BHyeonKim <rlaqhguse@gmail.com>
2024-11-06 14:14:31 +0900

    [#58]fix: 헤더바 수정

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,12 +1,13 @@
 import type {ReactNode} from 'react';
 
+import LoginButton from '@/app/components/LoginButton';
 import Footer from '@/components/layout/Footer';
 import LandingHeader from '@/components/layout/LandingHeader';
 
 const LandingLayout = ({children}: {children: ReactNode}) => {
   return (
     <>
-      <LandingHeader />
+      <LandingHeader rightComponent={<LoginButton />} />
       {children}
       <Footer />
     </>
