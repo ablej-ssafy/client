932ee1fe29682989f7b53029d489da0949f28792
BHyeonKim <rlaqhguse@gmail.com>
2024-11-07 12:04:03 +0900

    [#58]refactor: 네비게이션 바 수정

--- src/app/components/LoginButton/index.tsx
+++ src/app/components/LoginButton/index.tsx
@@ -1,19 +1,16 @@
 import {cookies} from 'next/headers';
 
-import {NavigationProps} from '@/components/layout/Navigation';
 import NavigationButton from '@/components/layout/NavigationButton';
 
-type LoginButtonProps = NavigationProps;
-
-const LoginButton = async (props: LoginButtonProps) => {
+const LoginButton = async () => {
   const isLoggedIn = cookies().get('authenticated');
 
   return isLoggedIn ? (
-    <NavigationButton href="/mypage" invert={props.invert}>
+    <NavigationButton href="/mypage" login>
       마이페이지
     </NavigationButton>
   ) : (
-    <NavigationButton href="/signin" outlined invert={props.invert} selected>
+    <NavigationButton href="/signin" login>
       로그인
     </NavigationButton>
   );
