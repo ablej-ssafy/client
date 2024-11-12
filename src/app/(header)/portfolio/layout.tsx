bb0b21ce10203372c714968af67c331a823c14f8
BHyeonKim <rlaqhguse@gmail.com>
2024-11-12 15:49:15 +0900

    [#58]feat: pdf 업로드 기능 추가

--- src/app/(header)/portfolio/layout.tsx
+++ src/app/(header)/portfolio/layout.tsx
@@ -1,18 +1,66 @@
 'use client';
 
 import classNames from 'classnames/bind';
-import {PropsWithChildren} from 'react';
+import {getCookie} from 'cookies-next';
+import {useRouter} from 'next/navigation';
+import {DragEvent, PropsWithChildren, useState} from 'react';
+import toast from 'react-hot-toast';
 
 import DragAndDropMenu from '@/features/portfolio/components/DragAndDropMenu';
+import ableJ from '@/services/ableJ';
 
 import styles from './layout.module.scss';
 
 const cx = classNames.bind(styles);
 
-const PortFolioLayout = ({children}: PropsWithChildren) => {
+const PortfolioLayout = ({children}: PropsWithChildren) => {
+  const router = useRouter();
+  const [isActive, setIsActive] = useState(false);
+
+  const handleDragEnter = (e: DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsActive(true);
+  };
+
+  const handleDragLeave = (e: DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    if (e.currentTarget.contains(e.relatedTarget as Node)) return;
+    setIsActive(false);
+  };
+
+  const handleDragOver = (e: DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+  };
+
+  const handleDropPdf = async (e: DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsActive(false);
+
+    const resume = e.dataTransfer.files[0];
+
+    if (resume.type !== 'application/pdf') {
+      toast.error('PDF 파일만 업로드 가능합니다.');
+      return;
+    }
+
+    const accessToken = getCookie('accessToken');
+
+    if (!accessToken) router.replace('/signin');
+
+    await ableJ.uploadResume(resume, accessToken as string);
+  };
+
   return (
     <div className={cx('container')}>
-      <main className={cx('main')}>{children}</main>
+      <main
+        className={cx('main', {active: isActive})}
+        onDragEnter={handleDragEnter}
+        onDragLeave={handleDragLeave}
+        onDragOver={handleDragOver}
+        onDrop={handleDropPdf}
+      >
+        {children}
+      </main>
       <aside className={cx('sidebar')}>
         <DragAndDropMenu />
       </aside>
@@ -20,4 +68,4 @@ const PortFolioLayout = ({children}: PropsWithChildren) => {
   );
 };
 
-export default PortFolioLayout;
+export default PortfolioLayout;
