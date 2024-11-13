330963512a651ca70226bf1a2b81a53d0d97e169
yunhayeon <yhy5049@naver.com>
2024-11-13 13:35:58 +0900

    [#45]fix: 깃허브 페이지 함수명 변경

--- src/app/(header)/mypage/github/page.tsx
+++ src/app/(header)/mypage/github/page.tsx
@@ -4,8 +4,8 @@ import styles from './page.module.scss';
 
 const cx = classNames.bind(styles);
 
-const ScrapPage = () => {
+const GithubPage = () => {
   return <main className={cx('page')}>깃허브 기능</main>;
 };
 
-export default ScrapPage;
+export default GithubPage;
