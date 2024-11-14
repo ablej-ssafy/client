ac04fb099f1d91fec326e6c20a6661ed8f30bdc4
BHyeonKim <rlaqhguse@gmail.com>
2024-11-14 21:59:07 +0900

    [#58]fix: 관심직무가 하나만 선택되도록 변경

--- src/features/auth/JobComboBox/index.tsx
+++ src/features/auth/JobComboBox/index.tsx
@@ -4,7 +4,6 @@ import {disassemble} from 'es-hangul';
 import {MouseEvent, useRef, useState} from 'react';
 
 import LabelWrapper from '@/components/common/LabelWrapper';
-import useClickOutside from '@/hooks/useClickOutside';
 import useJobs from '@/hooks/useJobs';
 import {Job} from '@/types/ableJ';
 
@@ -18,16 +17,14 @@ const JobComboBox = () => {
   const [selectedJob, setSelectedJob] = useState<Job['id']>();
   const [keyword, setKeyword] = useState('');
   const jobs = useJobs();
-  useClickOutside(ref, () => setIsOpen(false));
+  // useClickOutside(ref, () => setIsOpen(false));
 
   const handleSelectJob = (e: MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
     const jobId = +e.currentTarget.value;
-
-    if (selectedJob === jobId) {
-      setSelectedJob(undefined);
-      return;
-    }
     setSelectedJob(jobId);
+    setIsOpen(false);
+    console.log('event');
   };
 
   return (
@@ -43,38 +40,36 @@ const JobComboBox = () => {
           {jobs.find(job => job.id === selectedJob)?.name ||
             '직무를 선택해주세요.'}
         </button>
-        {isOpen && (
-          <div className={cx('dropdown')}>
-            <input
-              type="text"
-              placeholder="직무를 입력해주세요."
-              className={cx('combobox-input')}
-              onChange={e => setKeyword(e.currentTarget.value)}
-              value={keyword}
-            />
-            <ul className={cx('dropdown-list')}>
-              {!!jobs?.length &&
-                jobs
-                  .filter(job =>
-                    disassemble(job.name).includes(disassemble(keyword)),
-                  )
-                  .map(job => (
-                    <li key={job.id}>
-                      <button
-                        className={cx('dropdown-button', {
-                          selected: job.id === selectedJob,
-                        })}
-                        value={job.id}
-                        onClick={handleSelectJob}
-                        type="button"
-                      >
-                        {job.name}
-                      </button>
-                    </li>
-                  ))}
-            </ul>
-          </div>
-        )}
+        <div className={cx('dropdown', {open: isOpen})}>
+          <input
+            type="text"
+            placeholder="직무를 입력해주세요."
+            className={cx('combobox-input')}
+            onChange={e => setKeyword(e.currentTarget.value)}
+            value={keyword}
+          />
+          <ul className={cx('dropdown-list')}>
+            {!!jobs?.length &&
+              jobs
+                .filter(job =>
+                  disassemble(job.name).includes(disassemble(keyword)),
+                )
+                .map(job => (
+                  <li key={job.id}>
+                    <button
+                      className={cx('dropdown-button', {
+                        selected: job.id === selectedJob,
+                      })}
+                      value={job.id}
+                      onClick={handleSelectJob}
+                      type="button"
+                    >
+                      {job.name}
+                    </button>
+                  </li>
+                ))}
+          </ul>
+        </div>
       </div>
     </LabelWrapper>
   );
