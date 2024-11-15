2bdfff974e47af4878e0ebcd7ea2beaa62162bc6
yunhayeon <yhy5049@naver.com>
2024-11-15 16:33:54 +0900

    [#93]feat: 깃허브 분석 기능 구현 완료

--- src/app/(header)/mypage/github/result/page.tsx
+++ src/app/(header)/mypage/github/result/page.tsx
@@ -4,8 +4,9 @@ import Lottie from 'lottie-react';
 import Link from 'next/link';
 import {useRouter, useSearchParams} from 'next/navigation';
 import {useEffect} from 'react';
+import {MdOutlineArrowForwardIos} from 'react-icons/md';
 
-import animationData from '@/assets/lottie/SuccessfullyDone.json';
+import animationData from '@/assets/lottie/CheckOrigin.json';
 
 import styles from './result.module.scss';
 
@@ -22,21 +23,30 @@ const ResultPage = () => {
 
   return (
     <main className={styles.container}>
-      <div>
-        <Lottie
-          animationData={animationData}
-          loop={false}
-          autoplay={true}
-          style={{width: 140, height: 140}}
-        />
-      </div>
-      <div>
-        {project &&
-          `${decodeURIComponent(project)} 프로젝트에 대한 분석 요청이 완료되었습니다.`}
-      </div>
-      <div>분석이 완료되면 가입하신 이메일로 보고서가 자동 발송됩니다.</div>
-      <div>평균 소요 시간: 약 5분 (프로젝트 규모에 따라 상이)</div>
-      <Link href="/mypage/github">다른 프로젝트도 분석하기</Link>
+      {project && (
+        <>
+          <Lottie
+            animationData={animationData}
+            loop={false}
+            autoplay={true}
+            style={{width: 60, height: 60}}
+          />
+          <div className={styles.project}>
+            <span>{`${decodeURIComponent(project)}`}</span>
+            &nbsp;분석 요청이 완료되었습니다.
+          </div>
+          <div className={styles.script}>
+            <div>
+              분석이 완료되면 가입하신 이메일로 보고서가 자동 발송됩니다.
+            </div>
+            <div>평균 소요 시간: 약 5분 (프로젝트 규모에 따라 상이)</div>
+          </div>
+          <Link href="/mypage/github">
+            다른 프로젝트도 분석하기
+            <MdOutlineArrowForwardIos />
+          </Link>
+        </>
+      )}
     </main>
   );
 };
