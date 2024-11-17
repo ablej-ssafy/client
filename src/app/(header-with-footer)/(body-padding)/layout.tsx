0f156b9eb63d4f92ef896b368ece65f32b19d06c
김도영 <rlaehdud1002@naver.com>
2024-11-18 00:16:12 +0900

    [#113]fix: 카카오맵 오류 수정, 로그인 리다이렉트 오류 수정

--- src/app/(header-with-footer)/(body-padding)/layout.tsx
+++ src/app/(header-with-footer)/(body-padding)/layout.tsx
@@ -1,14 +1,13 @@
-import Script from 'next/script';
 import {ReactNode} from 'react';
 
 import styles from './layout.module.scss';
 
-const KAKAO_MAP_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_JS_KEY}&libraries=services&autoload=false`;
+// const KAKAO_MAP_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_JS_KEY}&libraries=services&autoload=false`;
 
 const PaddingLayout = ({children}: {children: ReactNode}) => {
   return (
     <div className={styles['child-container']}>
-      <Script strategy="beforeInteractive" src={KAKAO_MAP_URL}></Script>
+      {/* <Script strategy="afterInteractive" src={KAKAO_MAP_URL}></Script> */}
       {children}
     </div>
   );
