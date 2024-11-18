0b0c1936288c0b175d61c7d058a2d225818992c8
BHyeonKim <rlaqhguse@gmail.com>
2024-11-18 09:09:19 +0900

    feat: 이력서 화면에 사용자 이름 표시

--- src/features/resume/MyResume/index.tsx
+++ src/features/resume/MyResume/index.tsx
@@ -8,38 +8,39 @@ import styles from './myResume.module.scss';
 
 const MyResume = async () => {
   const cookieStore = cookies();
-  const token = cookieStore.get('accessToken')?.value;
+  const accessToken = cookieStore.get('accessToken')?.value;
 
-  if (!token) {
+  if (!accessToken) {
     console.log('Access Token이 없습니다.');
     return;
   }
 
-  const {data} = await ableJ.getResumeList(token);
+  const {data: resumeList} = await ableJ.getResumeList(accessToken);
+  const {data: profile} = await ableJ.getProfile(accessToken);
 
   return (
     <div className={styles.container}>
       <div className={styles['card-container-flex']}>
-        <h2>김보현님의 이력서</h2>
+        <h2>{profile.name}님의 이력서</h2>
         <div className={styles['card-container-grid']}>
           <MyResumeCard
             key={0}
             id={0}
-            url={''}
-            title={'포트폴리오'}
+            url=""
+            title="포트폴리오"
             date={getTodayDate()}
             type="포트폴리오"
           />
-          {!!data &&
-            data
+          {!!resumeList &&
+            resumeList
               .reverse()
-              .map(item => (
+              .map(resume => (
                 <MyResumeCard
-                  key={item.id}
-                  id={item.id}
-                  url={item.url}
-                  title={item.fileName}
-                  date={item.createdAt}
+                  key={resume.id}
+                  id={resume.id}
+                  url={resume.url}
+                  title={resume.fileName}
+                  date={resume.createdAt}
                 />
               ))}
         </div>
